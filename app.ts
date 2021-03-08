540ce2c7a361851c3267eaeb1088cedb8ee8c9b0
Mikhail Hursky <kakai747o@gmail.com>
2021-03-08 09:23:26 +0300

    feat: add nodemon

--- app.ts
+++ app.ts
@@ -1,9 +1,8 @@
-import createError from "http-errors";
 import * as express from "express";
 import * as cors from "cors";
 import { join } from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
+import * as cookieParser from "cookie-parser";
+import * as logger from "morgan";
 
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
@@ -19,20 +18,26 @@ app.use(cookieParser());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(
+  (
+    err: { message: any; status: any },
+    req: { app: { get: (arg0: string) => string } },
+    res: {
+      locals: { message: any; error: any };
+      status: (arg0: any) => void;
+      render: (arg0: string) => void;
+    },
+    next: any
+  ) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+  }
+);
 
 export default app;
